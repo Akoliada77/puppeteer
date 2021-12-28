999a449b023758166af850fbdebb34a4bffb3d7c
Alex Koliada <akoliada77@gmail.com>
2021-12-28 18:09:43 +0300

    new tests for dev solutions section

--- elements/MainPage/DevelopmentSolutions.js
+++ elements/MainPage/DevelopmentSolutions.js
@@ -7,6 +7,42 @@ let mainpage
 
 mainpage = new Mainpage()
 
+// variables for the section
+var sectionSelector = '.styles-module--container--34pZy'
+var titleSelector = '.styles-module--title--iIWVh.undefined'
+var subtitleSelector = '.styles-module--subTitle--1ToGn'
+var slideSelector = '.styles-module--slideWrapper--1e0Kl.slideWrapper'
+var circleButtonSelector = '.styles-module--largeCircleButton--1oaGQ.undefined.styles-module--size--2om9F'
+
 export default class DevelopmentSolutions {
-    async 
+    async isSectionDisplayed() {
+        expect(await isElementVisible(sectionSelector)).to.be.true
+    }
+    async sectionLocation() {
+        const gridContainerSelector = '.styles-module--container--1XxoU' 
+        const gridContainerBox = await (await page.$(gridContainerSelector)).boundingBox()
+        const sectionBox = await(await page.$(sectionSelector)).boundingBox()
+        expect(sectionBox.y).to.greaterThan(gridContainerBox.y)
+        expect(sectionBox.y).to.greaterThan(gridContainerBox.y+gridContainerBox.height)
+    }
+    async sectionContent() {
+        expect(await isElementVisible(titleSelector)).to.be.true
+        expect(await isElementVisible(subtitleSelector)).to.be.true
+        expect(await isElementVisible(slideSelector)).to.be.true
+        expect(await isElementVisible(circleButtonSelector)).to.be.true
+    }
+    async titleLocation() {
+        const titleBox = await(await page.$(titleSelector)).boundingBox()
+        const sectionBox = await(await page.$(sectionSelector)).boundingBox()
+        expect(titleBox.x).to.be.greaterThan(sectionBox.x)
+        expect(titleBox.y).to.be.equal(sectionBox.y)
+    }
+    async titleText() {
+        expect(await getText(page, titleSelector)).to.equal('Development solutions based on your business situation')
+    }
+    async subtitleLocation() {
+        const subtitleBox = await(await page.$(subtitleSelector)).boundingBox()
+        const titleBox = await(await page.$(titleSelector)).boundingBox()
+        expect()
+    }
 }
---
+++ tests/developmentSolutions.test.js
@@ -0,0 +1,32 @@
+import MainPage from '../pages/MainPage'
+import DevelopmentSolutions from '../elements/MainPage/DevelopmentSolutions'
+
+describe ('Development solutions', () => {
+    let mainpage 
+    let developmentsolutions
+   
+    jest.setTimeout(40000)
+
+    beforeAll(async() =>{
+        mainpage = new MainPage()
+        developmentsolutions = new DevelopmentSolutions()
+    })
+
+    it('Development solutions section is displayed', async () =>{
+        await mainpage.visit()
+        await developmentsolutions.isSectionDisplayed()
+    })
+    it('Development solutions location', async () =>{
+        await mainpage.visit()
+        await developmentsolutions.sectionLocation()
+    })
+    it('title of Development solution location', async () =>{
+        await mainpage.visit()
+        await developmentsolutions.titleLocation()
+    })
+    it('title of Development solution text', async () =>{
+        await mainpage.visit()
+        await developmentsolutions.titleText()
+    })
+
+})
