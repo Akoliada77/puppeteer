acc6ee4cb67a1be6ddd5c62df5224626a5974d6b
Alex Koliada <akoliada77@gmail.com>
2022-03-18 21:58:27 +0300

    adding variables

--- elements/MainPage/WhyWorkWithUs.js
+++ elements/MainPage/WhyWorkWithUs.js
@@ -9,8 +9,17 @@ mainpage = new Mainpage()
 
 // variables for the section
 var sectionSelector = '.styles-module--gridContainer--Zn72u.whyWorkWithUs__gridBlock_height '
-var titleSelector = '.styles-module--title--iIWVh.styles-module--headlineText--2tw0u'
-var subtitleSelector = ''
+var titleSelector = '.styles-module--headline--HXENq'
+var subtitleSelector = '#content > div > section:nth-child(4) > div.scrollmagic-pin-spacer > div > div > div.styles-module--description--3KUT5 > p'
+var moduleBlockSelector = '.undefined.styles-module--whyWorkWithUsBlock--3GNfC  '
+var firstImageSelector = '.styles-module--whyWork1--2SW3q'
+var secondImageSelector = '.styles-module--whyWork2--kQmgo'
+var thirdImageSelector = '.styles-module--whyWork3--v4CwA'
+var fourthImageSelector = '.styles-module--whyWork4--17Sb9'
+var fifthImageSelector = '.styles-module--whyWork5--pfbbH'
+var sixthImageSelector = '.styles-module--whyWork6--1eqp-'
+var seventhImageSelector = '.styles-module--whyWork7--MuQhS'
+var eighthImageSelevtor = '.styles-module--whyWork8--TTfu0'
 
 export default class WhyWorkWithUs {
     async isSectionDisplayed() {
@@ -22,4 +31,5 @@ export default class WhyWorkWithUs {
         const devSolutionsBox = await(await page.$(devSolutionsSelector)).boundingBox() 
         expect(sectionBox.y).to.be.greaterThan(devSolutionsBox.y)
     }
+    
 }
