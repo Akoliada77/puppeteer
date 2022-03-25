66e613f4d44db3c6db21fdcdb2d3905bc453947d
Alex Koliada <akoliada77@gmail.com>
2022-03-25 14:46:59 +0300

    adds test for number block section

---
+++ tests/numberBlock.test.js
@@ -0,0 +1,18 @@
+import MainPage from '../pages/MainPage'
+import NumbersBlock from '../elements/MainPage/NumbersBlock'
+
+describe ('Why work with us section', () => {
+    let mainpage 
+    let numbersBlock
+   
+    jest.setTimeout(40000)
+
+    beforeAll(async() =>{
+        mainpage = new MainPage()
+        numbersBlock = new NumbersBlock()
+    })
+    it('Section is displayed', async() =>{
+        await mainpage.visit()
+        await numbersBlock.isSectionDisplayed()
+    })
+})
