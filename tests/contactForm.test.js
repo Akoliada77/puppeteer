70186d1055a26e49c46e5dd1470755d1358ecb20
Alex Koliada <akoliada77@gmail.com>
2022-09-22 16:42:10 +0300

    new tests for contact form

--- tests/contactForm.test.js
+++ tests/contactForm.test.js
@@ -1,15 +1,18 @@
 import ContactPage from '../pages/ContactPage'
 import MainSection from '../elements/ContactPage/MainSection'
+import GridContainerContact from '../elements/ContactPage/GridContainerContact'
 
 describe ('Contact Form', () => {
     let contactpage 
     let mainsection
+    let gridcontainercontact
    
     jest.setTimeout(40000)
 
     beforeAll(async() =>{
         contactpage = new ContactPage()
         mainsection = new MainSection()
+        gridcontainercontact = new GridContainerContact()
     })
     it('Section is displayed', async() =>{
         await contactpage.visit()
@@ -26,6 +29,18 @@ describe ('Contact Form', () => {
     it('Check that first question is displayed', async () =>{
         await contactpage.visit()
         await mainsection.firstQuestionIsDisplayed()
+    })
+    it('Check href tag in links', async () =>{
+        await contactpage.visit()
+        await gridcontainercontact.checkSocialLinks()
+    })
+    it('Grid container is displayed', async () =>{
+        await contactpage.visit()
+        await gridcontainercontact.isSectionDisplayed()
+    })
+    it('Check email is correct', async () =>{
+        await contactpage.visit()
+        await gridcontainercontact.correctEmailDisplayed()
     })
 
 })
