3cdcda8ffbe8d3a45c7559b0d39533df46ae0c95
Alex Koliada <akoliada77@gmail.com>
2022-09-28 19:11:42 +0300

    framework addings for our work page

---
+++ pages/OurWork.js
@@ -0,0 +1,16 @@
+import { expect } from "chai"
+const { waitForText, shouldNotExist, click, getText, getTextXpath, getCount, typeText, selectText, isElementVisible, isXpathVisible } = require("../lib/helpers")
+const { generateID, generateEmail } = require("../lib/utils")
+const creds = require ('../creds.json')
+
+// variables for the section
+var casesSectionSelector = '.styles-module--wrapper--lJt7I.styles-module--offsetsContainer--5YIuy'
+
+export default class OurWork {
+    async visit() {
+        const url = 'https://rubyroidlabs.dev/case_study'
+        await page.goto(url)
+        expect(await isElementVisible(casesSectionSelector)).to.be.true
+        expect(page.url()).to.equal(url)
+    }
+}
