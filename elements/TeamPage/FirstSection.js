8fb08e103e445ee714670c6820dfffc460db8f79
Alex Koliada <akoliada77@gmail.com>
2022-10-24 20:45:32 +0300

    new tests for team page

--- elements/TeamPage/FirstSection.js
+++ elements/TeamPage/FirstSection.js
@@ -8,6 +8,7 @@ let ourwork
 // variables for the section
 var titleSelector = '.styles-module--title--dsH4E'
 var subTitleSelector = '.styles-module--subTitle--alz9U'
+var imageSelector = '.gbi--44973037-irgBWiX5HSUCjD7d9SW9nZ'
 
 ourwork = new TeamPage()
 
@@ -15,6 +16,8 @@ export default class FirstSection {
     async isSectionDisplayed() {
         expect(await isElementVisible(titleSelector)).to.be.true
         expect(await isElementVisible(subTitleSelector)).to.be.true
+        // expect(await isElementVisible(imageSelector)).to.be.true
     }
+
 
 }
