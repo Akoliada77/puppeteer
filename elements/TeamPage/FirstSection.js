56be32ce39a02467f592564952fbc78fefaddcde
Alex Koliada <akoliada77@gmail.com>
2022-10-27 13:52:55 +0300

    tests for team page

--- elements/TeamPage/FirstSection.js
+++ elements/TeamPage/FirstSection.js
@@ -8,7 +8,8 @@ let ourwork
 // variables for the section
 var titleSelector = '.styles-module--title--dsH4E'
 var subTitleSelector = '.styles-module--subTitle--alz9U'
-var imageSelector = '.gbi--44973037-irgBWiX5HSUCjD7d9SW9nZ'
+var imageSelector = '.gatsby-image-wrapper.gatsby-image-wrapper-constrained.styles-module--image--PVE0H'
+var breadCrumbsSelector = '.breadcrumb'
 
 ourwork = new TeamPage()
 
@@ -16,8 +17,12 @@ export default class FirstSection {
     async isSectionDisplayed() {
         expect(await isElementVisible(titleSelector)).to.be.true
         expect(await isElementVisible(subTitleSelector)).to.be.true
-        // expect(await isElementVisible(imageSelector)).to.be.true
     }
-
+    async sectionContent() {
+        expect(await isElementVisible(titleSelector)).to.be.true
+        expect(await isElementVisible(imageSelector)).to.be.true
+        expect(await isElementVisible(subTitleSelector)).to.be.true
+        expect(await isElementVisible(breadCrumbsSelector)).to.be.true
+    }
 
 }
